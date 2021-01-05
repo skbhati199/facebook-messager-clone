97f4107823ae46b1fe6d9b15d064f2ff091cf238
Sonu Kumar <skbhati199@gmail.com>
2021-01-05 22:40:00 +0530

    add

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from 'react'
+import './App.css'
+import {Button,makeStyles,InputLabel, FormControl, Input} from '@material-ui/core';
+import Message from './Message';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [messages, setMessages] = useState(['Hello world', 'Hi', 'Whats up']);
+  const [username,setUsername] = useState('');
+
+
+  useEffect(() => {
+    // run code here
+    // if its black  inside this code run
+    setUsername(prompt('Please enter your user name?'))
+  
+  }, [])
+
+  const sendMessage = (e) => {
+
+    e.preventDefault()
+    setMessages([...messages, inputValue])
+    setInputValue('')
+    console.log(messages)
+
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>Dream programming 😊🚀🚀🚀🚀🚀🚀🚀🚀</h1>
+        <h2>Welcome {username}</h2>
+        <form>
+          
+          <FormControl>
+            <InputLabel htmlFor="component-simple">Type here</InputLabel>
+            <Input id="component-simple" value={inputValue} onChange={event => setInputValue(event.target.value)} />
+          </FormControl>
+          <Button disabled={!inputValue} type="submit" onClick={sendMessage}  variant="contained" color="primary">Send Message</Button>
+        </form>
+        {
+          messages.map(value => {
+             return <Message text={value}/>
+          })
+        }
+       
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from "firebase";
+
+const firebaseApp = firebase.initializeApp({
+    apiKey: "AIzaSyAEelRqlLBE_wFmnZ8O7q9jDlLjmvTMNQw",
+    authDomain: "fir-app-6ac9e.firebaseapp.com",
+    databaseURL: "https://fir-app-6ac9e.firebaseio.com",
+    projectId: "fir-app-6ac9e",
+    storageBucket: "fir-app-6ac9e.appspot.com",
+    messagingSenderId: "749312866914",
+    appId: "1:749312866914:web:aa9cc81c27cd2412edaf3b"
+  });
+
+  const db = firebaseApp.firestore();
+
+  export {db,firebase}
